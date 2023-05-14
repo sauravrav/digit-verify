761c4a5f305eb25a67bb4a47ea5d5b3bbc449486
sauravb <s.bhatta2383@gmail.com>
2023-05-14 18:50:04 +0545

    Added client side validation

--- src/components/verify.js
+++ src/components/verify.js
@@ -1,15 +1,21 @@
-import {  useRef, useEffect } from 'react';
+import {  useRef, useEffect, useState } from 'react';
 
 function Verify() {
   const inputRefs = useRef([]);
+  const [hasError, setHasError] = useState(false); 
 
   const handleInput = (index, event) => {
     const input = event.target;
     const value = input.value;
-
-    // Move focus to next input field if current one is not empty
-    if (value !== "" && index < inputRefs.current.length - 1) {
-      inputRefs.current[index + 1].focus();
+    // Check if input value is numeric, and set error state accordingly
+    if (isNaN(value)) {
+      setHasError(true); // Add red border to input field
+    } else {
+      setHasError(false);
+      // Move focus to next input field if current one is not empty
+      if (value !== "" && index < inputRefs.current.length - 1 ) {
+        inputRefs.current[index + 1].focus();
+      }
     }
   };
 
@@ -40,23 +46,9 @@ function Verify() {
     event.preventDefault();
   };
 
-  const handleSubmit = async () => {
-    const response = await fetch('/api/verify', {
-      method: 'POST',
-      body: JSON.stringify({ code }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (response.ok) {
-      // redirect to success page on success
-      window.location.href = '/success';
-    } else {
-      // display error message on error
-      alert('Verification Error');
-    }
-  };
+  const handleSubmit = () => {
+    
+  }
 
   useEffect(() => {
     // Set focus to first input field when component mounts
@@ -66,6 +58,11 @@ function Verify() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="text-center">
+      {hasError && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+            <span className="block sm:inline">this is an error</span>
+          </div>
+        )}
         <h1 className="text-3xl font-bold mb-8">VERIFICATION CODE:</h1>
         <div className="flex justify-center mb-8">
             {[...Array(6)].map((_, index) => (
@@ -73,8 +70,8 @@ function Verify() {
                 type="text"
                 key={index}
                 maxLength="1"
-                className="w-10 h-10 mr-2 border-gray-400 border rounded text-center focus:outline-none focus:border-blue-500 "
                 ref={(el) => (inputRefs.current[index] = el)}
+                className={`w-10 h-10 mr-2 border-gray-400 border rounded text-center focus:outline-none ${hasError && isNaN(inputRefs.current[index].value) ? "border-red-500" : ""}`}
                 onChange={(event) => handleInput(index, event)}
                 onKeyDown={(event) => {
                   if (event.key === "Backspace") handleBackspace(index, event);
