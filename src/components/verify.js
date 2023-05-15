3853fc941bc3060171a228fcf8b7e95c5e1836f7
sauravb <s.bhatta2383@gmail.com>
2023-05-15 22:37:20 +0545

    Deploy error fix imr

--- src/components/verify.js
+++ src/components/verify.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import {  useRef, useEffect, useState } from 'react';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Verify from "@/components/verify"
 
 
